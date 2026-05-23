3e364bf8b7bde5ab0b7e6362e088c6caf004d3f1
aavelizp <105309180+aavelizp@users.noreply.github.com>
2026-05-23 14:12:23 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,7 @@
-// Configuración modificada para forzar compatibilidad con Vercel
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    server: { 
-      preset: "vercel", // ¡AQUÍ ESTÁ LA MAGIA! Obligamos al sistema a usar el motor de Vercel
-      entry: "server" 
-    },
+    server: { entry: "server" },
   },
 });
