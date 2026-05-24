acb53ffc84c6ae47d7066e34bc605c8896706ea8
aavelizp <aavelizp0107@gmail.com>
2026-05-24 14:06:40 -0400

    Corrigiendo llave API

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://timodswfbslxzgzsycwp.supabase.co";
 
-const supabaseAnonKey = "eyJhbeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpbW9kc3dmYnNseHpnenN5Y3dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NzIwNzksImV4cCI6MjA5NDU0ODA3OX0.0USxNfTd8Tms9SU72kkJWEXnL6N3MxFW7iXcsLMwJDQG";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpbW9kc3dmYnNseHpnenN5Y3dwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg5NzIwNzksImV4cCI6MjA5NDU0ODA3OX0.0USxNfTd8Tms9SU72kkJWEXnL6N3MxFW7iXcsLMwJDQ";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
