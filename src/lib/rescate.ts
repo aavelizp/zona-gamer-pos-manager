aacda8ca3571f4ee8e5031f1c310f06a32aaa827
null <null>
2026-08-07 21:19:45 -0400

    rescate

---
+++ src/lib/rescate.ts
@@ -0,0 +1,591 @@
+export const historial = {
+    "state": {
+      "rate": 900,
+      "queue": [],
+      "sales": [],
+      "combos": [],
+      "credits": [],
+      "matches": [
+        {
+          "id": "5ssj1loz",
+          "round": 1,
+          "isDraw": false,
+          "winnerId": "8h9een32",
+          "player1Id": "pb5ppdza",
+          "player2Id": "8h9een32",
+          "matchIndex": 0,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "uvic64w0",
+          "round": 1,
+          "isDraw": false,
+          "winnerId": "q95j2cce",
+          "player1Id": "q95j2cce",
+          "player2Id": "gt3cx29o",
+          "matchIndex": 1,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "fjf7xnbq",
+          "round": 1,
+          "isDraw": false,
+          "winnerId": "80jcy05s",
+          "player1Id": "80jcy05s",
+          "player2Id": "qpk6scjq",
+          "matchIndex": 2,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "6er3kcte",
+          "round": 2,
+          "isDraw": false,
+          "winnerId": "pb5ppdza",
+          "player1Id": "pb5ppdza",
+          "player2Id": "gt3cx29o",
+          "matchIndex": 3,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "srdwik51",
+          "round": 2,
+          "isDraw": false,
+          "winnerId": "8h9een32",
+          "player1Id": "8h9een32",
+          "player2Id": "qpk6scjq",
+          "matchIndex": 4,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "u0ce4ny6",
+          "round": 2,
+          "isDraw": false,
+          "winnerId": "80jcy05s",
+          "player1Id": "q95j2cce",
+          "player2Id": "80jcy05s",
+          "matchIndex": 5,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "3fn93c11",
+          "round": 3,
+          "isDraw": false,
+          "winnerId": "pb5ppdza",
+          "player1Id": "pb5ppdza",
+          "player2Id": "qpk6scjq",
+          "matchIndex": 6,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "78c336zq",
+          "round": 3,
+          "isDraw": false,
+          "winnerId": "80jcy05s",
+          "player1Id": "gt3cx29o",
+          "player2Id": "80jcy05s",
+          "matchIndex": 7,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "rci2n932",
+          "round": 3,
+          "isDraw": false,
+          "winnerId": "8h9een32",
+          "player1Id": "8h9een32",
+          "player2Id": "q95j2cce",
+          "matchIndex": 8,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "adzs7cll",
+          "round": 4,
+          "isDraw": false,
+          "winnerId": "pb5ppdza",
+          "player1Id": "pb5ppdza",
+          "player2Id": "80jcy05s",
+          "matchIndex": 9,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "mvb4yped",
+          "round": 4,
+          "isDraw": false,
+          "winnerId": "q95j2cce",
+          "player1Id": "qpk6scjq",
+          "player2Id": "q95j2cce",
+          "matchIndex": 10,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "lr9tkf94",
+          "round": 4,
+          "isDraw": false,
+          "winnerId": "8h9een32",
+          "player1Id": "gt3cx29o",
+          "player2Id": "8h9een32",
+          "matchIndex": 11,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "n2ru0ya2",
+          "round": 5,
+          "isDraw": false,
+          "winnerId": "pb5ppdza",
+          "player1Id": "pb5ppdza",
+          "player2Id": "q95j2cce",
+          "matchIndex": 12,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "pcu10ezd",
+          "round": 5,
+          "isDraw": false,
+          "winnerId": "8h9een32",
+          "player1Id": "80jcy05s",
+          "player2Id": "8h9een32",
+          "matchIndex": 13,
+          "tournamentId": "k9637m6k"
+        },
+        {
+          "id": "r64b6ynu",
+          "round": 5,
+          "isDraw": false,
+          "winnerId": "gt3cx29o",
+          "player1Id": "qpk6scjq",
+          "player2Id": "gt3cx29o",
+          "matchIndex": 14,
+          "tournamentId": "k9637m6k"
+        }
+      ],
+      "members": [
+        {
+          "id": "bb7yfb9c",
+          "name": "Yelfred Tlf",
+          "phone": "04244668743",
+          "createdAt": 1786057185496,
+          "lastVisit": 1786057185496,
+          "totalMinutes": 60,
+          "rewardMinutes": 60,
+          "pendingRewards": 0
+        }
+      ],
+      "soundOn": true,
+      "consoles": [
+        {
+          "id": "ps4-1",
+          "name": "PS4 #3",
+          "type": "PS4",
+          "charges": [],
+          "ratePerHour": 2,
+          "totalMinutes": 120,
+          "maintenanceMinutes": 120
+        },
+        {
+          "id": "ps4-2",
+          "name": "PS4 #4",
+          "type": "PS4",
+          "charges": [],
+          "ratePerHour": 2,
+          "totalMinutes": 180,
+          "maintenanceMinutes": 180
+        },
+        {
+          "id": "ps4-3",
+          "name": "PS4 #5",
+          "type": "PS4",
+          "charges": [],
+          "ratePerHour": 2,
+          "totalMinutes": 136,
+          "maintenanceMinutes": 136
+        },
+        {
+          "id": "ps4-4",
+          "name": "PS4 #6",
+          "type": "PS4",
+          "charges": [],
+          "ratePerHour": 2,
+          "totalMinutes": 0
+        },
+        {
+          "id": "ps5-1",
+          "name": "PS5 #1",
+          "type": "PS5",
+          "charges": [],
+          "ratePerHour": 3,
+          "totalMinutes": 120,
+          "maintenanceMinutes": 120
+        },
+        {
+          "id": "ps5-2",
+          "name": "PS5 #2",
+          "type": "PS5",
+          "charges": [],
+          "ratePerHour": 3,
+          "totalMinutes": 0
+        }
+      ],
+      "expenses": [],
+      "products": [
+        {
+          "id": "xgu305x4",
+          "name": "Pepsi 355ml",
+          "price": 1,
+          "stock": 24
+        }
+      ],
+      "tournaments": [
+        {
+          "id": "k9637m6k",
+          "game": "FIFA ",
+          "name": "TORNEO FC26 ",
+          "format": "league",
+          "status": "completed",
+          "entryFee": 5,
+          "createdAt": 1782079891236,
+          "dateRange": "27 jun.",
+          "maxPlayers": 20,
+          "prizePercentage": 70
+        },
+        {
+          "id": "9q3r4y42",
+          "game": "Mortal Kombat",
+          "name": "Torneo MK",
+          "format": "league",
+          "status": "registering",
+          "entryFee": 5,
+          "createdAt": 1784168393139,
+          "dateRange": "15/7/2026",
+          "maxPlayers": 999,
+          "prizePercentage": 60
+        },
+        {
+          "id": "daqg15cl",
+          "game": "FC 26",
+          "name": "Torneo FC 26 2",
+          "format": "league",
+          "status": "registering",
+          "entryFee": 5,
+          "createdAt": 1784758351996,
+          "dateRange": "22/7/2026",
+          "maxPlayers": 999,
+          "prizePercentage": 50
+        }
+      ],
+      "participants": [
+        {
+          "id": "pb5ppdza",
+          "phone": "04144271855",
+          "enrolledAt": 1782080001432,
+          "memberName": "David Santiago",
+          "enrollSaleId": "7vtatbqf",
+          "tournamentId": "k9637m6k",
+          "paymentStatus": "paid"
+        },
+        {
+          "id": "q95j2cce",
+          "enrolledAt": 1782080616848,
+          "memberName": "Frederick Venero",
+          "enrollSaleId": "howoa5yc",
+          "tournamentId": "k9637m6k",
+          "paymentStatus": "paid"
+        },
+        {
+          "id": "80jcy05s",
+          "phone": "04121044383",
+          "enrolledAt": 1783128057982,
+          "memberName": "Ulisses Meza",
+          "enrollSaleId": "vgqxnj81",
+          "tournamentId": "k9637m6k",
+          "paymentStatus": "paid"
+        },
+        {
+          "id": "qpk6scjq",
+          "enrolledAt": 1783128136734,
+          "memberName": "Eduardo Gonzalez",
+          "enrollSaleId": "13efcdes",
+          "tournamentId": "k9637m6k",
+          "paymentStatus": "paid"
+        },
+        {
+          "id": "gt3cx29o",
+          "phone": "04128443740",
+          "enrolledAt": 1783128156790,
+          "memberName": "Wilmer Olaizola",
+          "enrollSaleId": "t5eukwc0",
+          "tournamentId": "k9637m6k",
+          "paymentStatus": "paid"
+        },
+        {
+          "id": "8h9een32",
+          "phone": "04220777031",
+          "enrolledAt": 1783131012658,
+          "memberName": "Juan Cortes",
+          "enrollSaleId": "1elmuyq5",
+          "tournamentId": "k9637m6k",
+          "paymentStatus": "paid"
+        },
+        {
+          "id": "w11kfmoy",
+          "phone": "04144271855",
+          "enrolledAt": 1784168409940,
+          "memberName": "David Santiago",
+          "tournamentId": "9q3r4y42",
+          "paymentStatus": "pending"
+        },
+        {
+          "id": "i168p1fp",
+          "phone": "04144481122",
+          "enrolledAt": 1784254070897,
+          "memberName": "David Meza",
+          "tournamentId": "9q3r4y42",
+          "paymentStatus": "pending"
+        },
+        {
+          "id": "4ocwy6bb",
+          "phone": "04244286167",
+          "enrolledAt": 1784758384963,
+          "memberName": "Javier Garcia",
+          "tournamentId": "daqg15cl",
+          "paymentStatus": "pending"
+        }
+      ],
+      "pastClosures": [
+        {
+          "id": "1c9xyo3a",
+          "date": 1786077347322,
+          "sales": [
+            {
+              "id": "z1xi5tal",
+              "ts": 1785982598350,
+              "rate": 900,
+              "items": [
+                {
+                  "qty": 1,
+                  "name": "Prepago PS4 #5 (60 min)",
+                  "price": 2
+                }
+              ],
+              "total": 2,
+              "cashBs": 0,
+              "method": "full",
+              "cashUsd": 0,
+              "concept": "Consola",
+              "minutes": 60,
+              "mobileBs": 1800,
+              "consoleId": "ps4-3",
+              "mobileBank": "Venezuela",
+              "timeAmount": 2,
+              "consoleName": "PS4 #5",
+              "extrasAmount": 0
+            },
+            {
+              "id": "w21pvw7q",
+              "ts": 1786057185496,
+              "rate": 900,
+              "items": [
+                {
+                  "qty": 1,
+                  "name": "Prepago PS5 #1 (60 min)",
+                  "price": 3
+                }
+              ],
+              "total": 3,
+              "cashBs": 0,
+              "method": "full",
+              "cashUsd": 0,
+              "concept": "Consola",
+              "minutes": 60,
+              "customer": "Yelfred Tlf",
+              "mobileBs": 2700,
+              "consoleId": "ps5-1",
+              "mobileBank": "Banesco",
+              "timeAmount": 3,
+              "consoleName": "PS5 #1",
+              "extrasAmount": 0
+            },
+            {
+              "id": "z2my38l7",
+              "ts": 1786059236361,
+              "rate": 900,
+              "items": [
+                {
+                  "qty": 1,
+                  "name": "Prepago PS5 #1 (60 min)",
+                  "price": 3
+                }
+              ],
+              "total": 3,
+              "cashBs": 0,
+              "method": "full",
+              "cashUsd": 0,
+              "concept": "Consola",
+              "minutes": 60,
+              "mobileBs": 2700,
+              "consoleId": "ps5-1",
+              "mobileBank": "Mercantil",
+              "timeAmount": 3,
+              "consoleName": "PS5 #1",
+              "extrasAmount": 0
+            },
+            {
+              "id": "kvj8tnvt",
+              "ts": 1786061138731,
+              "rate": 900,
+              "items": [
+                {
+                  "qty": 1,
+                  "name": "Prepago PS4 #4 (60 min)",
+                  "price": 2
+                }
+              ],
+              "total": 2,
+              "cashBs": 0,
+              "method": "full",
+              "cashUsd": 0,
+              "concept": "Consola",
+              "minutes": 60,
+              "mobileBs": 1800,
+              "consoleId": "ps4-2",
+              "mobileBank": "Venezuela",
+              "timeAmount": 2,
+              "consoleName": "PS4 #4",
+              "extrasAmount": 0
+            },
+            {
+              "id": "rk1kwnnf",
+              "ts": 1786062903954,
+              "rate": 900,
+              "items": [
+                {
+                  "qty": 1,
+                  "name": "Prepago PS4 #5 (30 min)",
+                  "price": 1
+                }
+              ],
+              "total": 1,
+              "cashBs": 0,
+              "method": "full",
+              "cashUsd": 0,
+              "concept": "Consola",
+              "minutes": 30,
+              "mobileBs": 900,
+              "consoleId": "ps4-3",
+              "mobileBank": "Venezuela",
+              "timeAmount": 1,
+              "consoleName": "PS4 #5",
+              "extrasAmount": 0
+            }
+          ],
+          "expenses": [],
+          "totalSales": 11,
+          "totalExpenses": 0
+        },
+        {
+          "id": "1ylhwjyl",
+          "date": 1785899659708,
+          "sales": [
+            {
+              "id": "i68bncj2",
+              "ts": 1785887045644,
+              "rate": 900,
+              "items": [
+                {
+                  "qty": 1,
+                  "name": "Prepago PS4 #4 (120 min)",
+                  "price": 4
+                }
+              ],
+              "total": 4,
+              "cashBs": 0,
+              "method": "full",
+              "cashUsd": 0,
+              "concept": "Consola",
+              "minutes": 120,
+              "mobileBs": 3600,
+              "consoleId": "ps4-2",
+              "mobileBank": "Banesco",
+              "timeAmount": 4,
+              "consoleName": "PS4 #4",
+              "extrasAmount": 0
+            },
+            {
+              "id": "catcxhfo",
+              "ts": 1785890470346,
+              "rate": 900,
+              "items": [
+                {
+                  "qty": 1,
+                  "name": "Prepago PS4 #3 (60 min)",
+                  "price": 2
+                }
+              ],
+              "total": 2,
+              "cashBs": 0,
+              "method": "full",
+              "cashUsd": 0,
+              "concept": "Consola",
+              "minutes": 60,
+              "mobileBs": 1800,
+              "consoleId": "ps4-1",
+              "mobileBank": "Bancamiga",
+              "timeAmount": 2,
+              "consoleName": "PS4 #3",
+              "extrasAmount": 0
+            },
+            {
+              "id": "ncxn8aiu",
+              "ts": 1785896113638,
+              "rate": 900,
+              "items": [
+                {
+                  "qty": 1,
+                  "name": "Tiempo PS4 #5 (46 min)",
+                  "price": 1.5333333333333334
+                }
+              ],
+              "total": 1.5333333333333334,
+              "cashBs": 600,
+              "method": "mixed",
+              "cashUsd": 1,
+              "concept": "Consola",
+              "minutes": 46,
+              "mobileBs": 0,
+              "consoleId": "ps4-3",
+              "timeAmount": 1.5333333333333334,
+              "consoleName": "PS4 #5",
+              "extrasAmount": 0
+            },
+            {
+              "id": "pmxxg62y",
+              "ts": 1785896428731,
+              "rate": 900,
+              "items": [
+                {
+                  "qty": 1,
+                  "name": "Prepago PS4 #3 (30 min)",
+                  "price": 1
+                }
+              ],
+              "total": 1,
+              "cashBs": 0,
+              "method": "full",
+              "cashUsd": 1,
+              "concept": "Consola",
+              "minutes": 30,
+              "mobileBs": 0,
+              "consoleId": "ps4-1",
+              "timeAmount": 1,
+              "consoleName": "PS4 #3",
+              "extrasAmount": 0
+            }
+          ],
+          "expenses": [],
+          "totalSales": 8.533333333333333,
+          "totalExpenses": 0
+        }
+      ],
+      "sessionHistory": [],
+      "maintenanceLogs": []
+    },
+    "version": 0
+  }
